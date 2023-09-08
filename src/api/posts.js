b8d71d15230100e533506b68ea4e7d1738651cae
Muhammad <70463206+M-Saad-0@users.noreply.github.com>
2023-09-08 15:55:42 +0500

    Update posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://github.com/M-Saad-0/react-blog-post/blob/main/data/db.json",
-});
+const baseURL = "https://github.com/M-Saad-0/react-blog-post/blob/main/data/db.json";
+
+// Get the data from db.json
+const getData = async () => {
+  const response = await axios.get(baseURL);
+  return response.data;
+};
+
+// Post data to db.json
+const postData = async (data) => {
+  const response = await axios.post(baseURL, data);
+  return response.data;
+};
+
+// Delete data from db.json
+const deleteData = async (id) => {
+  const response = await axios.delete(`${baseURL}/${id}`);
+  return response.data;
+};
+
+export { getData, postData, deleteData };
